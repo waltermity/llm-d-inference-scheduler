c1d9e6952b467da0402c1eebf811fc4d0c63583d
Etai Lev Ran <elevran@gmail.com>
2025-05-07 14:59:21 +0300

    Load based scorer (#13)
    * align files with package comment and copyright
    
    Signed-off-by: Etai Lev Ran <elevran@gmail.com>
    
    * remove package comment
    
    Signed-off-by: Etai Lev Ran <elevran@gmail.com>
    
    * load aware scorer
    
    Signed-off-by: Etai Lev Ran <elevran@gmail.com>
    
    * load aware scorer test
    
    Signed-off-by: Etai Lev Ran <elevran@gmail.com>
    
    * fix: move base deploy to environments
    
    Signed-off-by: Shane Utt <shaneutt@linux.com>
    
    * feat: add deployment for istio crds
    
    Signed-off-by: Shane Utt <shaneutt@linux.com>
    
    * feat: add deploy for istio control plane
    
    Signed-off-by: Shane Utt <shaneutt@linux.com>
    
    * feat: add deploy for vllm simulator
    
    Signed-off-by: Shane Utt <shaneutt@linux.com>
    
    * feat: add deploy for inference gateway
    
    Signed-off-by: Shane Utt <shaneutt@linux.com>
    
    * Document gie fork workaround (#21)
    
    * chore: go mod tidy & verify
    
    Signed-off-by: Shane Utt <shaneutt@linux.com>
    
    * feat: add tmp makefile target to update gie fork
    
    Signed-off-by: Shane Utt <shaneutt@linux.com>
    
    * chore: update deps
    
    Signed-off-by: Shane Utt <shaneutt@linux.com>
    
    * docs: add fork workaround to README.md
    
    Signed-off-by: Shane Utt <shaneutt@linux.com>
    
    ---------
    
    Signed-off-by: Shane Utt <shaneutt@linux.com>
    
    * handle file comments
    
    Signed-off-by: Etai Lev Ran <elevran@gmail.com>
    
    * add mutating header to result
    
    Signed-off-by: Etai Lev Ran <elevran@gmail.com>
    
    ---------
    
    Signed-off-by: Etai Lev Ran <elevran@gmail.com>
    Signed-off-by: Shane Utt <shaneutt@linux.com>
    Co-authored-by: Maya Barnea <mayab@il.ibm.com>
    Co-authored-by: Ira Rosen <irar@il.ibm.com>
    Co-authored-by: Shane Utt <shaneutt@linux.com>

---
+++ pkg/scheduling/plugins/scorer/load_aware_scorer_test.go
@@ -0,0 +1,135 @@
+package scorer_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	k8stypes "k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/gateway-api-inference-extension/pkg/epp/backend"
+	backendmetrics "sigs.k8s.io/gateway-api-inference-extension/pkg/epp/backend/metrics" // Import config for thresholds
+	"sigs.k8s.io/gateway-api-inference-extension/pkg/epp/scheduling"
+	"sigs.k8s.io/gateway-api-inference-extension/pkg/epp/scheduling/plugins"
+	"sigs.k8s.io/gateway-api-inference-extension/pkg/epp/scheduling/plugins/picker"
+	"sigs.k8s.io/gateway-api-inference-extension/pkg/epp/scheduling/types"
+
+	"github.com/neuralmagic/llm-d-inference-scheduler/pkg/scheduling/plugins/scorer"
+)
+
+func TestLoadBasedScorer(t *testing.T) {
+	tests := []struct {
+		name    string
+		scorer  plugins.Scorer
+		req     *types.LLMRequest
+		input   []*backendmetrics.FakePodMetrics
+		wantRes *types.Result
+		err     bool
+	}{
+		{
+			name:   "load based scorer",
+			scorer: scorer.NewLoadAwareScorer(),
+			req: &types.LLMRequest{
+				Model:               "critical",
+				ResolvedTargetModel: "critical",
+				Critical:            true,
+			},
+			// pod2 will be picked because it has the shortest queue
+			input: []*backendmetrics.FakePodMetrics{
+				{
+					Pod: &backend.Pod{NamespacedName: k8stypes.NamespacedName{Name: "pod1"}},
+					Metrics: &backendmetrics.Metrics{
+						WaitingQueueSize:    2,
+						KVCacheUsagePercent: 0.2,
+						MaxActiveModels:     2,
+						ActiveModels: map[string]int{
+							"foo": 1,
+							"bar": 1,
+						},
+					},
+				},
+				{
+					Pod: &backend.Pod{NamespacedName: k8stypes.NamespacedName{Name: "pod2"}},
+					Metrics: &backendmetrics.Metrics{
+						WaitingQueueSize:    0,
+						KVCacheUsagePercent: 0.2,
+						MaxActiveModels:     2,
+						ActiveModels: map[string]int{
+							"foo": 1,
+							"bar": 1,
+						},
+					},
+				},
+				{
+					Pod: &backend.Pod{NamespacedName: k8stypes.NamespacedName{Name: "pod3"}},
+					Metrics: &backendmetrics.Metrics{
+						WaitingQueueSize:    5,
+						KVCacheUsagePercent: 0.2,
+						MaxActiveModels:     2,
+						ActiveModels: map[string]int{
+							"foo": 1,
+							"bar": 1,
+						},
+					},
+				},
+			},
+			wantRes: &types.Result{
+				TargetPod: &types.ScoredPod{
+					Pod: &types.PodMetrics{
+						Pod: &backend.Pod{NamespacedName: k8stypes.NamespacedName{Name: "pod2"}},
+						Metrics: &backendmetrics.Metrics{
+							WaitingQueueSize:    0,
+							KVCacheUsagePercent: 0.2,
+							MaxActiveModels:     2,
+							ActiveModels: map[string]int{
+								"foo": 1,
+								"bar": 1,
+							},
+							WaitingModels: map[string]int{},
+						},
+					},
+					Score: 0.5,
+				},
+				MutatedHeaders: map[string]string{},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			datastore := &fakeDataStore{pods: test.input}
+
+			scheduler := scheduling.NewSchedulerWithConfig(datastore, scheduling.NewSchedulerConfig(
+				[]plugins.PreSchedule{},
+				[]plugins.Filter{},
+				map[plugins.Scorer]int{
+					test.scorer: 1,
+				},
+				&picker.MaxScorePicker{},
+				[]plugins.PostSchedule{},
+			))
+			got, err := scheduler.Schedule(context.Background(), test.req)
+			if test.err != (err != nil) {
+				t.Errorf("Unexpected error, got %v, want %v", err, test.err)
+			}
+
+			opt := cmp.AllowUnexported(types.PodMetrics{})
+			if diff := cmp.Diff(test.wantRes, got, opt); diff != "" {
+				t.Errorf("Unexpected output (-want +got): %v", diff)
+			}
+		})
+	}
+}
+
+// TODO: this is probably better in upstream (e.g., epp/scheduling or epp/scheduling/plugins)
+type fakeDataStore struct {
+	pods []*backendmetrics.FakePodMetrics
+}
+
+// PodGetAll returns all pods in the store
+func (fds *fakeDataStore) PodGetAll() []backendmetrics.PodMetrics {
+	pm := make([]backendmetrics.PodMetrics, 0, len(fds.pods))
+	for _, pod := range fds.pods {
+		pm = append(pm, pod)
+	}
+	return pm
+}
