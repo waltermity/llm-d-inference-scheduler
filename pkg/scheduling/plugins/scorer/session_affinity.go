0454fa8478f86c598a5b403b8cea7b225d5e14fb
Maya Barnea <mayab@il.ibm.com>
2025-05-19 14:48:22 +0300

    provide additional information when error occures in session scorer's PostResponse
    Signed-off-by: Maya Barnea <mayab@il.ibm.com>

--- pkg/scheduling/plugins/scorer/session_affinity.go
+++ pkg/scheduling/plugins/scorer/session_affinity.go
@@ -65,7 +65,11 @@ func (s *SessionAffinity) Score(ctx *types.SchedulingContext, pods []types.Pod) 
 // Tracked in https://github.com/llm-d/llm-d-inference-scheduler/issues/28
 func (s *SessionAffinity) PostResponse(ctx *types.SchedulingContext, pod types.Pod) {
 	if ctx.Resp == nil || pod == nil || pod.GetPod() == nil {
-		ctx.Logger.V(logutil.DEBUG).Info("Session affinity scorer - skip post response because one of ctx.Resp, pod, pod.GetPod is nil")
+		reqID := "updefined"
+		if ctx.Resp != nil {
+			reqID = ctx.Resp.RequestId
+		}
+		ctx.Logger.V(logutil.DEBUG).Info("Session affinity scorer - skip post response because one of ctx.Resp, pod, pod.GetPod is nil", "req id", reqID)
 		return
 	}
 
