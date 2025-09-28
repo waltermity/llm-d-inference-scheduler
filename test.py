feaafb5b678e4e0451ba31bad782928edaace087
David Walthier <walthierdavid42@gmail.com>
2025-09-28 14:32:58 +0200

    script for testing endpoints

---
+++ test.py
@@ -0,0 +1,39 @@
+import requests
+
+gateway_url="http://localhost:30080"
+
+try:
+    endpoint = f"{gateway_url}/v1/models"
+    headers = {"Content-Type": "application/json"}
+    
+    r = requests.get(endpoint, headers=headers, timeout=10)
+    r.raise_for_status()
+    response = r.json()
+
+    print("Inference result:", response)
+
+    #---------------Completion---------------------
+    completion_endpoint = f"{gateway_url}/v1/completions"
+    payload = {"model": "food-review", "prompt": "How are you today?"}
+    resp = requests.post(completion_endpoint, headers=headers, json=payload, timeout=30)
+    resp.raise_for_status()
+    print("\n\nCompletion result:", resp.json())
+except Exception as e:
+    print(f"Error: {e}")
+
+try:
+    #---------------Mistral 7B Instruct---------------------
+    completion_endpoint = f"{gateway_url}/v1/completions"
+    payload = {
+        "model": "mistralai/Mistral-7B-Instruct-v0.2",
+        "prompt": "hi",
+        "max_tokens": 16,
+        "temperature": 0
+    }
+    resp = requests.post(completion_endpoint, headers=headers, json=payload, timeout=30)
+    resp.raise_for_status()
+    print("\n\nMistral completion result:", resp.json())
+except Exception as e:
+    print(f"Error: {e}")
+
+    
